c734da9170537b86cd3c4499e986a1a8aa89de6a
dmitry <stremous@list.ru>
2016-12-10 20:21:14 +0300

    add logic to get JSON and render the data add hide JQery logic

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-var response = [];
+(function () {
 
-$.getJSON("random.json", function(result) {
-  console.log(result);
-});
+  $(document).ready(function(e) {
+    var response = [],
+        testBlockHtml = '',
+        testBlock = $('#test-block');
+
+    $.getJSON('random.json?id=5', function (data) {
+      response = data;
+
+      activateBlock();
+    });
+
+    function activateBlock() {
+      response.forEach(function (item) {
+        testBlockHtml += '<li>' + item.company + ' ' + item.phone + '</li>';
+      });
+      testBlock.html(testBlockHtml);
+    }
+
+  });
+
+})();
+
